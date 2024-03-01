292f077338383f784c041bd6026c58edc1aa7175
Jayden Wong <jaywong8288@gmail.com>
2024-02-29 19:17:33 -0800

    changes menu layout

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # PythonTextEditor
  Simple Python text editor
- based on a [tutorial](https://www.instructables.com/Create-a-Simple-Python-Text-Editor/) from PumpkinSmasher
+ based on tutorials from [PumpkinSmasher](https://www.instructables.com/Create-a-Simple-Python-Text-Editor/) and [Make with Data](https://www.youtube.com/watch?v=xqDonHEYPgA)
+
+
+ ## Current functionality:
+ - New
+ - Save
+ - Save As
+ - Open
+ - Quit
+ - Change font types
+
+## Planned features:
+- version control (i.e. undo, redo)
+- change font size
--- texteditor.py
+++ texteditor.py
@@ -1,22 +1,35 @@
 from tkinter import *
 from tkinter import filedialog
-from tkinter import IntVar
+from tkinter.messagebox import showerror
 
-root = Tk("Text Editor")
+filename = None
 
-text=Text(root)
-text.grid()
+def newFile():
+    global filename
+    filename = "Untitled"
+    text.delete(0.0, END)
 
-def saveas():
-    global text
-    t = text.get("1.0", "end-1c")
-    savelocation = filedialog.asksaveasfilename()
-    file1 = open(savelocation, "w+")
+def saveFile():
+    global filename
+    t = text.get(0.0, END)
+    file1 = open(filename, "w")
     file1.write(t)
     file1.close()
 
-button = Button(root, text="Save", command=saveas)
-button.grid()
+def saveAs():
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    t = text.get(0.0, END)
+    try:
+        f.write(t.rstrip())
+    except:
+        showerror(title="Oops!", message="Unable to save file...")
+
+
+def openFile():
+    f = filedialog.askopenfile(mode='r')
+    t = f.read()
+    text.delete(0.0, END)
+    text.insert(0.0, t)
 
 def FontHelvetica():
     global text
@@ -26,13 +39,39 @@ def FontCourier():
     global text
     text.config(font="Courier")
 
-font=Menubutton(root, text="Font")
-font.grid()
-font.menu = Menu(font, tearoff=0)
-font["menu"]=font.menu
-helvetica=IntVar()
-courier=IntVar()
-font.menu.add_checkbutton(label="Helvetica", variable=helvetica, command=FontHelvetica)
-font.menu.add_checkbutton(label="Courier", variable=courier, command=FontCourier)
+def FontTimes():
+    global text
+    text.config(font="Times")
 
+def FontArial():
+    global text
+    text.config(font="Arial")
+
+root = Tk()
+root.title("Text Editor")
+root.minsize(width=400, height=400)
+root.maxsize(width=400, height=400)
+
+text = Text(root, width=400, height=400)
+text.pack()
+
+menubar = Menu(root)
+filemenu = Menu(menubar)
+filemenu.add_command(label="New", command=newFile)
+filemenu.add_command(label="Open", command=openFile)
+filemenu.add_command(label="Save", command=saveFile)
+filemenu.add_command(label="Save As...", command=saveAs)
+filemenu.add_separator()
+filemenu.add_command(label="Quit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+fontmenu = Menu(menubar)
+fontmenu.add_command(label="Helvetica", command=FontHelvetica)
+fontmenu.add_command(label="Courier", command=FontCourier)
+fontmenu.add_command(label="Times", command=FontTimes)
+fontmenu.add_command(label="Arial", command=FontArial)
+menubar.add_cascade(label="Font", menu=fontmenu)
+
+
+root.config(menu=menubar)
 root.mainloop()
